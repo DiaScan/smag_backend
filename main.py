ffbd00ab0c46a4decee934e7c8765a85b72d9a65
Ashmin Jayson <ashminjayson10@gmail.com>
2023-09-22 23:05:13 +0530

    chore: updates for deployment

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn
-from store import db
 from mltoolkit import apriori
+import os
+from store import db
+
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def run_init():
     try:
         import env
+        print('Running init')
         env.init()
     except (e) as e:
         print(e)
-    db.init()
+    db.init_client()
+
 
 def run_tests():
-    # print(db.get_all_shops().data)
+    print(db.get_all_shops().data)
+    print(db.get_all_users().data)
     return
 
 
@@ -29,8 +50,23 @@ async def frequent_patterns():
     return frequent_patterns
 
 
+class User(BaseModel):
+    username: str
+    password: str
+
+
+@app.post('/register_user')
+async def register_user(user: User):
+    res = db.add_new_user(user.username, user.password)
+    return {"message": res}
+
+
+@app.post('/login_user')
+async def login_user():
+    pass
+
 
 if __name__ == '__main__':
-    # run_init()
-    run_tests()
+    run_init()
+    # run_tests()
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- store/db.py
+++ store/db.py
@@ -1,16 +1,19 @@
 import os
+import postgrest
 from supabase import create_client, Client
 
-SUPABASE_PUBLIC_URL : str = ''
-SUPABASE_SERVICE_ROLE : str = ''
+SUPABASE_PUBLIC_URL: str = ''
+SUPABASE_SERVICE_ROLE: str = ''
+
 supabase: Client = None
 
 
-def init():
+def init_client():
     global SUPABASE_PUBLIC_URL, SUPABASE_SERVICE_ROLE, supabase
     SUPABASE_PUBLIC_URL = os.environ.get('SUPABASE_PUBLIC_URL')
     SUPABASE_SERVICE_ROLE = os.environ.get('SUPABASE_SERVICE_ROLE')
-    supabase = create_client(supabase_url=SUPABASE_PUBLIC_URL, supabase_key=SUPABASE_SERVICE_ROLE)
+    supabase = create_client(
+        supabase_url=SUPABASE_PUBLIC_URL, supabase_key=SUPABASE_SERVICE_ROLE)
 
 
 def get_all_shops():
@@ -18,5 +21,37 @@ def get_all_shops():
     return shop_records
 
 
+def get_all_users():
+    user_records = supabase.table('user').select("*").execute()
+    return user_records
 
 
+def user_exists(username):
+    try:
+        response = supabase.table('user').select(
+            '*').eq('username', username).execute()
+
+        if len(response.data) != 0:
+            return True
+        else:
+            return False
+    except postgrest.exceptions.APIError as err:
+        print(err.message)
+        return False
+
+
+def add_new_user(username: str, password: str):
+    init_client()
+    try:
+        if user_exists(username):
+            return "user already exists"
+
+        supabase.table('user').insert({
+            'username': username,
+            'password': password
+        }).execute()
+
+        return "user successfully added"
+    except postgrest.exceptions.APIError as err:
+        print(err.message)
+        return "error adding user"
