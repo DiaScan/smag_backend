87c6b8aab384dc41328c96cbe776953448f87a65
Ashmin Jayson <ashminjayson10@gmail.com>
2023-09-23 00:38:54 +0530

    fix: updated host for render deploy

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ async def login_user(user: User):
 if __name__ == '__main__':
     run_init()
     # run_tests()
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
